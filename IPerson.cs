0f2ec38956f0323277d24ab81fb9ce4fe176c442
s66829020@gmail.com <s66829020@gmail.com>
2023-08-01 23:21:13 +0530

    first commit

---
+++ IPerson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP_Interface
+{
+    public interface IPerson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+    }
+}
